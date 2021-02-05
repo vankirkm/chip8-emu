9dba6269dd88cd0fd483ee41a2f75390d468637e
Michael Van Kirk <vankirk.6@wright.edu>
2021-02-05 16:30:16 -0500

    setting initial variables

--- src/main/Chip8.java
+++ src/main/Chip8.java
@@ -1,7 +1,22 @@
 package main;
 
 public class Chip8 {
-    //testing git ssh
+    private short opcode;
+    private char[] memory;
+    private char cReg[];
+    private short iReg;
+    private short pCount;
+    private char[] gfx;
+    private char delayTimer;
+    private char soundTimer;
+    private short[] stack;
+    private short sPoint;
+    private char[] key;
 
-
+    public Chip8(){
+        this.pCount = 0x200;
+        this.opcode = 0;
+        this.iReg = 0;
+        this.sPoint = 0;
+    }
 }
