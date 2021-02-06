e80341afcca0def33e94520b34962ac9d4474801
Michael Van Kirk <vankirk.6@wright.edu>
2021-02-06 12:47:24 -0500

    more ssh testing

--- src/main/main.java
+++ src/main/main.java
@@ -3,6 +3,7 @@ package main;
 public class main {
 
     public static void main(String[] args){
+        //more ssh testing
         String filename = "Pong.ch8";
         Chip8 newChip = new Chip8();
         newChip.loadGame(filename);
