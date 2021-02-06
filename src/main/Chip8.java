28ed8a51e8360a96030c8f0eb7ad7b9004f0ee7e
Michael Van Kirk <vankirk.6@wright.edu>
2021-02-06 16:59:47 -0500

    laying out basic opcode decoding structure

--- src/main/Chip8.java
+++ src/main/Chip8.java
@@ -20,21 +20,21 @@ public class Chip8 {
     char[] fontSet =
     {
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-                0x20, 0x60, 0x20, 0x20, 0x70, // 1
-                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-                0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
     };
 
     public Chip8(){
@@ -71,9 +71,69 @@ public class Chip8 {
     }
 
     public void emulateCycle(){
-        System.out.println(Integer.toBinaryString(memory[pCount]));
-        System.out.println(Integer.toBinaryString(memory[pCount+1]));
+        //get opcode
         this.opcode = (short) (memory[pCount] << 8 | memory[pCount + 1]);
-        System.out.println(Integer.toBinaryString(this.opcode));
+        System.out.println(Integer.toHexString(this.opcode & 0xF000));
+        //decode opcode
+        switch(this.opcode & 0xF000){
+
+            //multiple cases where the first 4 bits of the opcode are 0, so break into another switch to look at
+            //the last two bytes of the opcode
+            case 0x0000:
+                break;
+
+            // 1nnn - set pCount to nnn
+            case 0x1000:
+                break;
+
+            // 2nnn - increment sPoint, then put the current pCount on top of the stack. Then set pCount to nnn.
+            case 0x2000:
+                break;
+
+            // 4xkk - compare the register Vx to kk and if the are not equal, increment pCount by 2
+            case 0x4000:
+                break;
+
+            // 5xy0 - compare register Vx to Vy and if they are equal, increment pCount by 2
+            case 0x5000:
+                break;
+
+            // 6xkk - put the value kk into register Vx
+            case 0x6000:
+                System.out.println("opcode found");
+                break;
+
+            // 7xkk - add the value of kk to the value of register Vx, then store the result in Vx
+            case 0x7000:
+                break;
+
+            //multiple cases where the first 4 bits of the opcode are 8, so break into another switch to look at
+            //the last 4 bits of the opcode
+            case 0x8000:
+                break;
+
+            // 9xy0 - compare values of Vx and Vy and if they are not equal, increment pCount by 2
+            case 0x9000:
+                break;
+
+            // Annn - set iReg to nnn
+            case 0xA000:
+                break;
+
+            // Bnnn - set pCount to nnn plus the value of V0
+            case 0xB000:
+                break;
+
+            // Cnnn - generate a random number from 0 - 255 and perform AND operation on it and the value kk.
+            // store the results in Vx
+            case 0xC000:
+                break;
+
+            // Dnnn - set iReg to nnn
+            case 0xD000:
+                break;
+
+        }
+
     }
 }
