92471f093cc68249e2b3d5b106f9a56d7a627030
Michael Van Kirk <vankirk.6@wright.edu>
2021-02-06 12:44:31 -0500

    testing binary file input

--- src/main/Chip8.java
+++ src/main/Chip8.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Chip8 {
     private short opcode;
     private char[] memory;
@@ -25,7 +30,21 @@ public class Chip8 {
         this.key = new char[16];
     }
 
-    public void loadGame(){
+    public void loadGame(String filename){
+        try{
+            FileInputStream fis = new FileInputStream("roms/" + filename);
+            int ch = 0;
+            try{
+                while((ch = fis.read()) != -1){
+                    System.out.println(ch);
+                }
+            }catch (IOException e){
+                System.out.println("End of file unexpectedly reached.");
+            }
+
+        }catch(FileNotFoundException e){
+            System.out.println("File does not exist. Check file name and try again.");
+        }
 
 
     }
--- src/main/main.java
+++ src/main/main.java
@@ -1,4 +1,10 @@
 package main;
 
 public class main {
+
+    public static void main(String[] args){
+        String filename = "Pong.ch8";
+        Chip8 newChip = new Chip8();
+        newChip.loadGame(filename);
+    }
 }
